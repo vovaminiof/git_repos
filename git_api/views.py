5781a7bf3f9b2dd94963c7747243544a7b19ac4a
vovaminiof <vovaminiof@yahoo.com>
2013-07-26 15:26:28 +0300

    added .gitignore

--- git_api/views.py
+++ git_api/views.py
@@ -4,18 +4,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-	if request.method == 'POST':
-		query = request.POST['query']
-		users = []
-		try:
-			g = Github()
-			repo = g.get_repo(query)
-			for user in repo.get_contributors():
-				obj = {'id': user.id, 'name' : user.name, 'login' : user.login, 'link' : user.html_url}
-				users.append(obj)
-		except Exception as ex:
-			return HttpResponse('Currently unavailable')
-		
-		return render(request, 'index.html', {'users':users})
+    if request.method == 'POST':
+        query = request.POST['query']
+        users = []
+        try:
+            g = Github()
+            repo = g.get_repo(query)
+            for user in repo.get_contributors():
+                obj = {'id': user.id, 'name' : user.name, 'login' : user.login, 'link' : user.html_url}
+                users.append(obj)
+        except Exception as ex:
+            return HttpResponse('Currently unavailable')
+        
+        return render(request, 'index.html', {'users':users})
 
-	return render(request,'index.html')
+    return render(request,'index.html')
